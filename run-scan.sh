6f51fe8e0cd668feebe03f46391621508661acbc
Maarten Boot <11140490+maarten-boot@users.noreply.github.com>
2025-12-02 12:47:03 +0100

    script testing

--- run-scan.sh
+++ run-scan.sh
@@ -1,8 +1,64 @@
 #! /usr/bin/env bash
 
-pip install rl-deploy
+install_rl_protect()
+{
+    pip install rl-protect
+    rl-protect --version
+}
 
-rl-deploy --version
+install_rl_deploy()
+{
+    pip install rl-deploy
+    rl-deploy --version
+}
 
-echo "description=This is a message"  >> $GITHUB_OUTPUT
-echo "status=success"                 >> $GITHUB_OUTPUT
+show_params()
+{
+    if [ "${RL_VERBOSE}" ]
+    then
+        cat <<!
+Params:
+    RL_ITEM_TO_SCAN:    ${RL_ITEM_TO_SCAN}
+    RL_PROFILE:         ${RL_PROFILE}
+    RL_LOG_FILE:        ${RL_LOG_FILE}
+    RL_CHECK_DEPS:      ${RL_CHECK_DEPS}
+    RL_CONCISE:         ${RL_CONCISE}
+    RL_TOKEN:           ${RL_TOKEN} # remove later
+    RL_SERVER:          ${RL_SERVER}
+    RL_PROXY_SERVER:    ${RL_PROXY_SERVER}
+    RL_PROXY_PORT:      ${RL_PROXY_PORT}
+    RL_PROXY_USER:      ${RL_PROXY_USER}
+    RL_PROXY_PASSWORD:  ${RL_PROXY_PASSWORD}
+    RL_RETURN_STATUS:   ${RL_RETURN_STATUS}
+    RL_NO_COLOR:        ${RL_NO_COLOR}
+    RL_VERBOSE:         ${RL_VERBOSE}
+!
+    fi
+}
+
+run_scan()
+{
+    # rl-protect ${RL_ITEM_TO_SCAN} &2>2 | tee 1
+    # intercept output from 1 and 2
+
+    OUT_DESCRIPTION="THIS IS A DESCRIPTION MESSAGE"
+    OUT_STATUS="success"
+}
+
+set_output()
+{
+    echo "description=${OUT_DESCRIPTION}"  >> $GITHUB_OUTPUT
+    echo "status=${OUT_STATUS}"                 >> $GITHUB_OUTPUT
+}
+
+main()
+{
+    install_rl_deploy
+    # install_rl_protect
+
+    show_params
+    run_scan
+    set_output
+}
+
+main
